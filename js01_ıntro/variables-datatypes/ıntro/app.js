44d2940734060703b9ba56862992dee6e5bd3683
nurantezer <nurantezer68@gmail.com>
2023-05-01 07:19:47 +0300

    update

--- js01_ıntro/variables-datatypes/ıntro/app.js
+++ js01_ıntro/variables-datatypes/ıntro/app.js
@@ -10,6 +10,17 @@ console.log(sayi)
 console.log(typeof sayi)
 
 
-const sayi1 = "33"
+const sayi1 = "33" //Tirnak içindekiler string(text) olarak atanır
 console.log(sayi1)
 console.log(typeof sayi1)
+
+
+// sayi = 88 //!Assignment to constant variable.
+
+
+// const pi; //! Missing initializer in const
+pi = 3.14
+ 
+const s1 = "hello"
+const s2 = "fs13"
+console.log(s1 + s2);
