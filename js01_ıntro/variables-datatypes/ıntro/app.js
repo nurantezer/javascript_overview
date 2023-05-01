089299bfde23bd4d235d5bdedfa7ba60e5e9aa1d
nurantezer <nurantezer68@gmail.com>
2023-05-01 08:32:55 +0300

    scope

--- js01_ıntro/variables-datatypes/ıntro/app.js
+++ js01_ıntro/variables-datatypes/ıntro/app.js
@@ -21,6 +21,45 @@ console.log(typeof sayi1)
 // const pi; //! Missing initializer in const
 pi = 3.14
  
-const s1 = "hello"
+const s1 = "hello "
 const s2 = "fs13"
-console.log(s1 + s2);
+console.log(s1 + s2);//? String Concationation
+// JS default olarak + operatöründe eğer değişkenlerden en az birisi
+//string ise bunları birbirine ekler
+
+const n1 = "5"
+const n2 = 10
+console.log(n1 + n2);//? String Concationation
+
+//! Ayni alan (scope) icerisinde ayni isimle yeniden degisken tanimlanamaz.
+// const n1 = 5;
+
+// n2 = n2 + 1; //! error
+
+//? ======================  LET  ========================
+//* LET de CONST gibi yaygın kullanılan değişken tanımlama yöntemidir.
+//* CONST'dan farkı, sadece tanımlama kısmında değil istenildiği zaman değeri değiştirilebilir.
+//* CONST gibi tanımlandığı blok içerisinde geçerlidir. Başka yerlerden erişilemez. (Block-Scoped)
+//* CONST kullanamadığımız durumlarda (değişkenin değeri sürekli değişecekse) LET kullanmalıyız.
+
+let dil = "javascript"
+console.log(dil);
+
+dil = "java"
+console.log(dil);
+
+dil = true
+console.log(dil, typeof dil); 
+
+dil = 3.14
+console.log(dil, typeof dil);
+
+let cohort;
+console.log(cohort); //* undefined(tanımsız, type'ı yok)
+
+cohort = 3
+console.log(typeof cohort); //? number
+
+// let cohort = "fs13" //!SyntaxError: Identifier 'cohort' has already been declared (at app.js:60:5)
+//! Ayni alan (scope) icerisinde ayni isimle yeniden degisken tanimlanamaz.
+
