89b69f21e4cb1e99d761f53c6d15804ded49fc66
nurantezer <nurantezer68@gmail.com>
2023-05-01 10:38:08 +0300

    NaN

--- js02_datatype-operators/operators.js
+++ js02_datatype-operators/operators.js
@@ -27,4 +27,14 @@ const firstName = "Ahmet"
 const LastName = "Can"
 console.log(firstName + " " + LastName);
 
-const s1 = 5, s2 = 7, s3 = "5"
+const s1 = 5, s2 = "4", s3 = "three"
+console.log(s1 + s2); //? 54
+console.log(s1 - s2); //? 1
+console.log(s1 - s3); //? NaN (Not A Number)
+//! String ifadeyi numbera çevirmeye çalıştı fakat mümkün olmadığı için NaN
+//! döndürdü
+
+const difference = s1 - s3
+console.log(difference, typeof NaN); //? NaN'in data type'ı number'dır
+
+
