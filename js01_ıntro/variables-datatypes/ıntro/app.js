17e8437f53156ae670174df22ce33a37b5bf9d2c
nurantezer <nurantezer68@gmail.com>
2023-05-01 06:38:10 +0300

    typeof

--- js01_ıntro/variables-datatypes/ıntro/app.js
+++ js01_ıntro/variables-datatypes/ıntro/app.js
@@ -1,3 +1,15 @@
 console.log("***** Data Types *****");
 
 //*  CONST ve LET, ECMASCRIPT6 ile gelmiştir. VAR eski versiyonlardan beri bulunmaktadır.
+
+const fs = "Cohort 13"
+console.log(fs)
+
+const sayi = 20
+console.log(sayi)
+console.log(typeof sayi)
+
+
+const sayi1 = "33"
+console.log(sayi1)
+console.log(typeof sayi1)
