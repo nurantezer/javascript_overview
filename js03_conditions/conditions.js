2cf23c1c0de68ef6ae62a6abce00e90798e7cbda
nurantezer <nurantezer68@gmail.com>
2023-05-02 04:43:40 +0300

    example 1

---
+++ js03_conditions/conditions.js
@@ -0,0 +1,19 @@
+// * =======================================================
+// *             Karar Yapilari (Conditions)
+// * =======================================================
+
+console.log(" **** Conditions ****")
+
+//? ORNEK1: Console'dan girilen bir sayisinin pozitif,negatif
+//? yada 0 oldugunu tespit ederek yazdiriniz.
+
+// const num = Number(prompt("Please enter a number:"))
+// console.log(typeof num)
+
+// if (num > 0) {
+//     console.log(`${num} is positive`)
+// } else if (num < 0) {
+//     console.log(`${num} is negative`)
+// } else {
+//     console.log(`${num} is equal to zero`)
+// }
