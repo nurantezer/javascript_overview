ec3f7f02247ddc8be360be217585dfd4bc75f83b
nurantezer <nurantezer68@gmail.com>
2023-05-02 15:31:33 +0300

    example 6

--- js03_conditions/example/example.js
+++ js03_conditions/example/example.js
@@ -103,9 +103,9 @@
 
 
 
-//* Soru-5 Artık yıl sorusu
-//* Artık yıl, bir yıldaki günlerin 365 yerine 366 gün olmasına denir. 
-//* Bu fazladan gün(artık gün), normalde 28 gün olan şubat ayına 29 Şubat'ın eklenmesi ile elde edilir.  
+//* Soru-6 Artık yıl sorusu
+//* Artık yıl, bir yıldaki günlerin 365 yerine 366 gün olmasına denir.
+//* Bu fazladan gün(artık gün), normalde 28 gün olan şubat ayına 29 Şubat'ın eklenmesi ile elde edilir.
 //* Bunun hesaplanması için aşağıdaki algoritma kullanılır.
 
 //* Bir tarih 4 ile tam bölünüp 100 katları ile tam bölünemiyorsa
@@ -113,3 +113,13 @@
 //* artık yıllarda 29 gün olarak belirlenir.
 
 //* Yukarıdaki bilgilere göre bir yılın artık yıl olup olmadığını konsola yazdıran conditional statement'i yazınız
+
+const yil = +prompt('lütfen bir yıl giriniz')
+
+if (yil % 400 === 0) {
+    console.log(`${yil} artık yıldır`)
+} else if (yil % 4 === 0 && yil % 100 !== 0) {
+    console.log(`${yil} artık yıldır`)
+} else {
+    console.log(`${yil} artık yıl değildir`)
+}
