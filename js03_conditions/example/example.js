ae519ac8392d02e10964de02c7c3ec4efc88f9a6
nurantezer <nurantezer68@gmail.com>
2023-05-02 14:23:27 +0300

    example 2

--- js03_conditions/example/example.js
+++ js03_conditions/example/example.js
@@ -1,9 +1,16 @@
 //*Soru-1 Bir sayının tek mi çift mi olduğunu consola yazdıran koşulu yazınız
 
-const sayi = 33
+// const sayi = 33
 
-if (sayi % 2 === 0) {
-    console.log(`${sayi} bir çift sayıdır`)
-} else if(sayi % 2 === 1 || -1) {
-    console.log(`${sayi} bir tek sayıdır`)
-}
+// if (sayi % 2 === 0) {
+//     console.log(`${sayi} bir çift sayıdır`)
+// } else if(sayi % 2 === 1 || -1) {
+//     console.log(`${sayi} bir tek sayıdır`)
+// }
+
+//*Soru-2 Bir sayının 0 ile 100 arasında olup olmadığını konsola yazdıran
+//*ternary yapııyı yazınız
+
+// const sayi = 555
+
+// 0 < sayi && sayi < 100 ?  console.log('sağlar') : console.log('sağlamaz') 
