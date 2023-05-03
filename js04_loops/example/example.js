0c9e33e70a026f6fdbf3567229594fd37b1fad95
nurantezer <nurantezer68@gmail.com>
2023-05-03 13:26:30 +0300

    example 1

---
+++ js04_loops/example/example.js
@@ -0,0 +1,11 @@
+//* Soru-1 1000 ile 2000 arasındaki 13 e bölümünden 3 kalan sayıları
+//* konsola yazınız
+
+for (let i = 1000; i < 2000; i++) {
+  if (i % 13 === 3) {
+    console.log(i);
+  }
+}
+
+
+
