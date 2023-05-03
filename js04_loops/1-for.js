ff93a6de576682c39fb995f56320edac86024908
nurantezer <nurantezer68@gmail.com>
2023-05-03 05:21:58 +0300

    example

--- js04_loops/1-for.js
+++ js04_loops/1-for.js
@@ -1,3 +1,26 @@
 // * =======================================================
 // *             FOR LOOP
 // * =======================================================
+
+console.log(" **** FOR ****")
+
+//? Ornek: 1 den n kadar sayilari toplayan kodu yaziniz.
+
+// const n = Number(prompt("Enter a number"))
+
+// let sum = 0      //*toplayacağımız değişkenleri aktarmak için
+
+// for (let i = 1; i <= n; i++){
+//     sum += i
+// }
+// console.log("SUM:", sum)
+
+//? Ornek: 0-100 Arasinda n adet rasgele tamsayi sayi ureten kodu
+//? for dongusu ile yaziniz..
+
+const num = Number(prompt("Enter a number"))
+
+for (let j = 1; j <= num; j++) {
+    const randomNumber = Math.round(Math.random() * 100)
+    console.log(`${j}. random number:${randomNumber}`)
+}
---
+++ js04_loops/README.md
@@ -0,0 +1,13 @@
+                          DÖNGÜ(İTERASYON)
+1- for
+2- do-while() ve while()
+3- for of, for in => Dizi, object ---Data structure--
+4- ForEach, map, filter, reduce...
+
+FOR
+- Birşeyi ekrana 100 kere bastırmak
+for(let i = 1 ; i <= 100 ; i++){
+    başlangıç    koşul    artırım,azaltım
+    değeri                    miktarı
+}
+
