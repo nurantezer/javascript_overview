b8d57e0b9dac2e123d71dfecef628ffdda6e848e
nurantezer <nurantezer68@gmail.com>
2023-05-03 06:18:22 +0300

    prime number

--- js04_loops/1-for.js
+++ js04_loops/1-for.js
@@ -18,9 +18,39 @@ console.log(" **** FOR ****")
 //? Ornek: 0-100 Arasinda n adet rasgele tamsayi sayi ureten kodu
 //? for dongusu ile yaziniz..
 
-const num = Number(prompt("Enter a number"))
+// const num = Number(prompt("Enter a number"))
 
-for (let j = 1; j <= num; j++) {
-    const randomNumber = Math.round(Math.random() * 100)
-    console.log(`${j}. random number:${randomNumber}`)
+// for (let j = 1; j <= num; j++) {
+//     const randomNumber = Math.round(Math.random() * 100)
+//     console.log(`${j}. random number:${randomNumber}`)
+// }
+
+//* NOT:
+//* Math.floor() =>en yakin alt tamsayiya yuvarlar
+//* Math.ceil() =>en yaki ust tamsayiya yuvarlar
+//* Math.round() => degerine gore yuvarlar.
+//* Math.trunc() => kesirli sayinin tam kismini alir.
+
+//? ÖRNEK: Girilen bir sayinin Asal olup olmadigini yazdiran kodu
+//? for dongulerini kullanarak yaziniz.
+
+const primeNum = Number(prompt("Enter a number"))
+
+let isPrime = true
+
+if (primeNum < 2) {
+    console.log("Number shoul be bigger than 1")
+} else {
+    for (let i = 2; i < primeNum; i++){
+    if (primeNum % i === 0) {
+         //? tam bölünüyorsa asal değildir
+        console.log(`${primeNum} is not a prime number`)
+        isPrime = false
+      }    
+    }
+    
+    if (isPrime === true) {
+    console.log(`${primeNum} is a prime number`)
+  }
 }
+
