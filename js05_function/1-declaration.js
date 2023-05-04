cb8310fad41d666b0e56630a9d9d12b832988600
nurantezer <nurantezer68@gmail.com>
2023-05-04 05:17:41 +0300

    declaration

--- js05_function/1-declaration.js
+++ js05_function/1-declaration.js
@@ -6,3 +6,39 @@ console.log("** FUNC DECLARATION **")
 
 //* ORNEK1:
 //*****************************************/
+
+//? Declaration
+function printHello() {
+    console.log("Hello")
+}
+
+printHello() //! invoke, call
+
+//* ORNEK2:
+//*****************************************/
+
+function greet(firstName, lastName) {
+    console.log(`Hi, ${firstName} ${lastName}`)
+}
+
+//? invoke examples
+greet("Sabata", "2288")
+greet("Sercan", 'Yılmaz')
+greet("Nuran", "Tezer")
+
+const name = prompt("your name:")
+const surname = prompt("your surname")
+greet(name, surname)  //*pass by value değişkenin isminin bir önemi yok
+                      //*önemli olan value
+//? js de fonksiyonlar parameterlere degiskenleri degil onlarin degerlerinin aktarir.
+// !greet(lastName, firstName)
+
+greet("Ahmet")
+greet("Mehmet")
+
+//! Bir parametreyi cagirma sirasinda kullanmaz isek onun
+//! yerine default parametre atayabiliriz. Ornekteki lastName
+//! parametresi icin default değer olarak '' atanmistir.
+function greet(firstName, lastName= "") {
+    
+}
---
+++ js05_function/README.md
@@ -0,0 +1,10 @@
+                 **** FONKSİYONLAR ****
+- Tekrar azaltıyor.
+- Büyük görevleri alt görevlere bölüyor.
+- declaration => calling-invoking.
+- RAM e geliyor o da stack alanında saklıyor.
+- 1. Function Declaration
+function topla(argüman/parametre listesi){
+                                                 deklarasyon kısmı
+}
+topla() => invoke kısmı
