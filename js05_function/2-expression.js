cbd3f1d91137bec8b72407109dbf64522fd22916
nurantezer <nurantezer68@gmail.com>
2023-05-04 08:50:01 +0300

    nested function

--- js05_function/2-expression.js
+++ js05_function/2-expression.js
@@ -52,5 +52,46 @@ console.log(`The Biggest:${findBiggest(-3, -5)}`)
 console.log(`The Biggest:${findBiggest(-3, -5, 6)}`)
 console.log(`The Biggest:${findBiggest(-5, -3)}`)
 
-//*-Number.MAX_VALUE 3.sayı girilmediğinde negatiflerde çalışmayı 
+//*-Number.MAX_VALUE 3.sayı girilmediğinde negatiflerde çalışmayı
 //*garanti ettik.
+
+//* ORNEK3: Bir fonksiyon icerisinde baska fonksiyonlar cagrilabilir
+//**************************************************/
+
+const add = function (n1, n2) {
+  return n1 + n2
+}
+const sub = function (n1, n2) {
+  return n1 - n2
+}
+const div = function (n1, n2) {
+  return n1 / n2
+}
+const mul = function (n1, n2) {
+  return n1 * n2
+}
+
+const compute = function (n1, n2, op) {
+    let result = 0
+
+    switch (op) {
+        case '+':
+            result = add(n1,n2)
+            break;
+        case '-':
+            result = sub(n1,n2)
+            break;
+        case '/':
+            result = div(n1,n2)
+            break;
+        case '-':
+            result = mul(n1,n2)
+            break;
+        default:
+            break;
+    }
+    return result
+}
+console.log(compute(4,3,"/"))
+console.log(compute(2,4,"*"))
+console.log(compute(3,1,"+"))
