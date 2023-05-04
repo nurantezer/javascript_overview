ffbae2aeb553c1fc0410af4e31cbc8eb06f4517d
nurantezer <nurantezer68@gmail.com>
2023-05-04 11:12:34 +0300

    recursive

---
+++ js05_function/4-recursive.js
@@ -0,0 +1,13 @@
+// ? ========================================
+// ?             RECURSION
+// ? ========================================
+
+//* Kendi kendi cagiran fonksiyona recursive fonksiyon denilir.
+//* Ozellikle karmasik matematiksel hesaplamalarin kodlamasini
+//* kolaylaştirmak icin kullanilir.
+//* Ancak, dikkatli kullanmak gerekir. Yoksa stack overflow hatasi alinabilir.
+
+//* ORNEK: Girilen n. terime kadar Fibonacci sayilarinin toplamini
+//* hesaplayarak yazdiran fonksiyonu recursive olarak kodlayiniz.
+
+console.log("*** 4 - RECURSION ***")
