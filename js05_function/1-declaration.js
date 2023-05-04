1a244fabaa1c04c4005a28c91dd664b6846bae9f
nurantezer <nurantezer68@gmail.com>
2023-05-04 06:32:07 +0300

    declaration

--- js05_function/1-declaration.js
+++ js05_function/1-declaration.js
@@ -17,28 +17,80 @@ printHello() //! invoke, call
 //* ORNEK2:
 //*****************************************/
 
-function greet(firstName, lastName) {
-    console.log(`Hi, ${firstName} ${lastName}`)
-}
+// function greet(firstName, lastName) {
+//     console.log(`Hi, ${firstName} ${lastName}`)
+// }
 
-//? invoke examples
-greet("Sabata", "2288")
-greet("Sercan", 'Yılmaz')
-greet("Nuran", "Tezer")
+// //? invoke examples
+// greet("Sabata", "2288")
+// greet("Sercan", 'Yılmaz')
+// greet("Nuran", "Tezer")
 
-const name = prompt("your name:")
-const surname = prompt("your surname")
-greet(name, surname)  //*pass by value değişkenin isminin bir önemi yok
-                      //*önemli olan value
-//? js de fonksiyonlar parameterlere degiskenleri degil onlarin degerlerinin aktarir.
-// !greet(lastName, firstName)
+// const name = prompt("your name:")
+// const surname = prompt("your surname")
+// greet(name, surname)  //*pass by value değişkenin isminin bir önemi yok
+//                       //*önemli olan value
+// //? js de fonksiyonlar parameterlere degiskenleri degil onlarin degerlerinin aktarir.
+// // !greet(lastName, firstName)
 
-greet("Ahmet")
-greet("Mehmet")
+// greet("Ahmet")
+// greet("Mehmet")
 
-//! Bir parametreyi cagirma sirasinda kullanmaz isek onun
-//! yerine default parametre atayabiliriz. Ornekteki lastName
-//! parametresi icin default değer olarak '' atanmistir.
-function greet(firstName, lastName= "") {
+// //! Bir parametreyi cagirma sirasinda kullanmaz isek onun
+// //! yerine default parametre atayabiliriz. Ornekteki lastName
+// //! parametresi icin default değer olarak '' atanmistir.
+// function greet(firstName, lastName= "") {
     
+// }
+
+//* ORNEK3:
+//******************************************/
+
+// function calculate(name, yob) {
+//     const age = new Date().getFullYear() - yob
+//     console.log(`${name}'s age is ${age}`)
+// }
+
+// calculate("elif", 2000)
+
+
+function calculate(name, yob) {
+    const age = new Date().getFullYear() - yob
+    return`${name}'s age is ${age}`
 }
+//*return dedim ama bana direk birşey döndürmüyor niye çünkü bir
+//*bir değişkene atayıp yazmadım.64,65 satırdaki gibi yazdırabilirim.
+
+//? Ornek olmasi icin yazildi. return programdan ana programa dönmesi
+// function x(s1, s2) {
+//   return (s1 + s2) / (s1 * s2)
+// }
+
+const elif = calculate("elif", 2000)
+console.log(elif)
+console.log(calculate("Rose", 1990))
+
+//* ORNEK4:
+//*******************************************/
+
+//? Console'dan girilen bir sayinin tek veya cift olddgunu
+//? hesaplayan ve sonucu ana programa donduren kodu fonksiyonlar
+//? ile yaziniz.
+
+function oddOrEven(num) {
+    return num % 2 ? "ODD" : "EVEN" 
+    // return num % 2 === 0 ? "EVEN" : "ODD"
+}
+
+  //? Alternative
+  // let result
+  // if (num % 2 === 0) {
+  //   result = "EVEN"
+  // } else {
+  //   result = "ODD"
+  // }
+  // return result;
+// }
+const number = +prompt("Please enter a number to check odd or even")
+const oddEven = oddOrEven(number)
+console.log(`${number} is ${oddEven}`)
--- js05_function/README.md
+++ js05_function/README.md
@@ -8,3 +8,14 @@ function topla(argüman/parametre listesi){
                                                  deklarasyon kısmı
 }
 topla() => invoke kısmı
+- Return : Fonksiyonlar kodları ana programa döndürmek isteyebilir.
+
+funciton topla(s1, s2) {
+    const toplam = s1+s2
+    return toplam
+    return s1+s2  iki şekilde de olur
+    clg(age) bunun bir anlamı yok çünkü işlemi ve kendisi ana programa döndürür
+}
+- return olmazsa çağırdığımda undifened verir.
+const x = topla(2,3) Programın alt taraflarında x'i istediğim gibi kullanabilirim.
+
