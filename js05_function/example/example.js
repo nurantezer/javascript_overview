b4e4f8231d784b5ad17c9723bc5ab98d5b11612d
nurantezer <nurantezer68@gmail.com>
2023-05-05 09:48:16 +0300

    example 1

--- js05_function/example/example.js
+++ js05_function/example/example.js
@@ -1,1 +1,9 @@
+//*Soru-1) Celsius değerini Fahrenheit değerine dönüştüren bir fonksiyon yazınız
+//*(celsius*9/5)+32 = Fahrenheit
+//*console.log(calFahrenheit(30)) // output : 86
 
+function calFahrenheit(cel) {
+    const fahr = cel * (9/5) + 32
+    return cel + ` derece` + " " + fahr + " fahrenheit'a eşittir"
+}
+console.log(calFahrenheit(65))
