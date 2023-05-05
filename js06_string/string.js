1dba41ed464969e1e79e9100e03945172a2e2e24
nurantezer <nurantezer68@gmail.com>
2023-05-05 06:29:10 +0300

    concat

--- js06_string/string.js
+++ js06_string/string.js
@@ -9,8 +9,17 @@ const str4 = str1 + str2
 console.log(str4)
 
 console.log(str4.concat(str3)) //*birleştirme yapar clarusway helloworld
-const str5 = new String(`Non-primitive String`)//!object
+const str5 = new String(`Non-primitive String`)//object
 console.log(str5, typeof str5)
 //*Normalde primitive veri tiplerinin property veya metodu olmaz. Ancak
-//*javascript otomatik olarak primitive stringler için object wrapper 
+//*javascript otomatik olarak primitive stringler için object wrapper
 //*oluşturur.
+
+//!concat()-immutable
+const s1 = "hello"
+const s2 = "world"
+const s3 = s1.concat(s2)
+console.log(s3) //helloworld
+console.log(s3.concat("clarusway", str2, "merhaba"))//helloworldclarusway hellomerhaba
+console.log(s2, s3)
+
