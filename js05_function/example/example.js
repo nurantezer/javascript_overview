0a7fe290a71c88926d04f52f53569aed0b0b6ddb
nurantezer <nurantezer68@gmail.com>
2023-05-05 10:35:25 +0300

    example 3

--- js05_function/example/example.js
+++ js05_function/example/example.js
@@ -22,3 +22,27 @@ function reverseString(str) {
 }
 
 console.log(reverseString('hello world'))
+
+//*Soru3 Belirli bir string ifadenin palindrome olup olmadığını bulan
+//*fonksiyonu yazınız.(Büyük, küçük harf duyarlılığı olmamalıdır)Ayna
+//*etkisi ile ortadan bölmesi
+//*const string = 'selles'
+//*console.log(isPalindrome(string)) // output: true
+
+const string = 'seles'
+
+function reverseString(str) {
+  let str2 = ``;
+  for (let i = str.length - 1; i >= 0; i--) {
+    str2 += str[i];
+  }
+  return str2;
+}
+
+const isPalindrome = function (string) {
+    const reversed = reverseString(string)
+    return reversed == string 
+        ? `${string} bir palindrome`
+        : `${string} palendrome değil`
+}
+console.log(isPalindrome(string))
