fae02657e5857971271482dc45845d96a31ed767
nurantezer <nurantezer68@gmail.com>
2023-05-05 10:09:24 +0300

    example 2

--- js05_function/example/example.js
+++ js05_function/example/example.js
@@ -7,3 +7,18 @@ function calFahrenheit(cel) {
     return cel + ` derece` + " " + fahr + " fahrenheit'a eşittir"
 }
 console.log(calFahrenheit(65))
+
+//*Soru-2) Belirli bir string ifadenin tersini bulan fonksiyonu yazınız
+//*console.log(reverseString('hello world)) // output: dlrow olleh
+
+let str = 'Selamun Aleykum'
+
+function reverseString(str) {
+    let str2 = ''
+    for (let i = str.length - 1; i >= 0; i--){
+        str2 += str[i]
+    }
+    return str2
+}
+
+console.log(reverseString('hello world'))
