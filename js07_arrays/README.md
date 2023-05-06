d5a4831170edad78f827be6d230d16ca8b90a27a
nurantezer <nurantezer68@gmail.com>
2023-05-06 05:17:52 +0300

    yöntemler

--- js07_arrays/README.md
+++ js07_arrays/README.md
@@ -11,3 +11,6 @@ person.age
 
 Stack-oturma odası-     Heap-salon-  referans salonun içindeki belirli koltuğu gösterir.
 kisiler[1] stacke gidiyor referans adresini bulup heape gidiyor.
+
+- object wrapper metali suya batırıp altın görünümü aldırmak.Diğer dillerde biz yapıyoruz ama js insiyatif alıp kendisi yapıyor
+- object HZ.Adem classı gibi temel class objecten array (OOP bu)
--- js07_arrays/arrays.js
+++ js07_arrays/arrays.js
@@ -6,3 +6,44 @@ console.log("***** ARRAYS ******")
 
 //* Dizi Tanimlama
 //*-----------------------------------------------
+// ! 1.Yöntem (Array Literal) - Tercih edilen yöntem
+
+const names = ["emir", "oğuzhan", "levent", "tarık"]
+
+console.log(names)
+//? 4 (names dizisinin length propertisine erişiyoruz)
+//* non-rimitive verilerde özellikler olabilir ve bunlara erişim . nokta
+//* notasyonu ile olabilir.
+console.log(names.length)
+
+const x = 5.234 //? primitive
+console.log(x.toFixed(2)) //? object wrapper sayesinde
+//? Primitive'lerde normal sartlarda property olmaz.
+
+//? Ancak JS kullanimi kolaylastirmak icin bazi durumlarda
+//? primitive verileri bir obje sarmlayicisi ile sarmalar.
+
+console.log(typeof names)//? Js de array aslında object dediğimiz base objecten
+//? türetilmiştir.
+
+const emptArr = []
+console.log(emptArr)
+
+// ! 2.Yöntem (Object Constructor)
+
+const codingLangs = new Array("C", "C++", "JS", "GO")
+console.log(codingLangs)
+
+const numbers = new Array(3, 2, 1)
+console.log(numbers) //? (3) [3, 2, 1]
+
+const number1 = new Array(10) //*demekki bellekte on tane boş bir yer ayır
+console.log(number1)
+
+// ! 3.Yöntem (Array.of())
+const number2 = Array.of(1, 2, 3)
+console.log(number2) //?(3) [1, 2, 3]
+
+const numbers3 = Array.of(5)
+console.log(numbers3) //? [5]
+
