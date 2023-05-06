f21b73a31545fae407f919a1474a03860f15e892
nurantezer <nurantezer68@gmail.com>
2023-05-06 11:12:38 +0300

    iteration

---
+++ js07_arrays/array_iteration/2-iteration.js
@@ -0,0 +1,10 @@
+//* =======================================
+//*           FOREACH METHOD
+//* =======================================
+
+console.log("****** ITERATION **********")
+
+//?-------------- ÖRNEK -------------------
+//?  Dizideki herbir fiyati konsola bastiriniz.
+
+const prices = [100, 250, 50, 89]
