f9ed250920906206da43eda6dffe56892ee6fb19
nurantezer <nurantezer68@gmail.com>
2023-05-07 13:50:03 +0300

    example 2

--- js06_string/example/example.js
+++ js06_string/example/example.js
@@ -14,3 +14,14 @@ function emailValidate(email) {
 }
 console.log(emailValidate(email))
 
+//*Soru-2 Bir dizideki tüm elemanların ortalmasını hesaplayan bir javascript
+//*fonksiyonu yazınız. Test Verisi: console.log(average([1,2,3,4,5,])) 3
+
+let dizi = [1, 2, 3, 4, 5]
+
+function average(dizi) {
+    let sum = 0
+    dizi.forEach((num) => (sum += num))
+    return sum / dizi.length
+}
+console.log(average(dizi))
