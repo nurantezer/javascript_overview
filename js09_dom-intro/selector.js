bf3efcc604436cde1b56c96685f29fb9af3319c8
nurantezer <nurantezer68@gmail.com>
2023-06-02 21:05:19 +0300

    ıd

--- js09_dom-intro/selector.js
+++ js09_dom-intro/selector.js
@@ -10,3 +10,13 @@ console.log(myHeader)
 
 myHeader.style.color = "blue"
 myHeader.style.backgroundColor = "yellow"
+
+const button = document.getElementById("btn")
+const textInput = document.getElementById("input")
+button.style.backgroundColor = "black"
+button.style.color = "yellow"
+button.style.padding = "0.7rem"
+button.style.border = "none"
+button.style.borderRadius = "10px"
+button.style.fontSize = "20px"
+textInput.style.padding = "0.7rem"
